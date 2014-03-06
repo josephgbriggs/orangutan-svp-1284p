17aaaedd2fb7388b346fc990ec077f6a4e3a9223
Joseph Briggs <joseph.g.briggs@acm.org>
2014-03-06 11:09:45 -0600

    version of led program using the registers directly

---
+++ led-run-reg/led-run-reg.c
@@ -0,0 +1,45 @@
+#include <avr/io.h>
+#include <pololu/OrangutanTime.h>
+
+/*
+ * led-run-reg: for the Orangutan SVP.
+ *
+ * This program will run signals through pins A0 - A7 in a cycle.
+ * LEDs placed in those port pins will make a nice show.
+ *
+ * This program is the same as led-run, but it is implemented
+ * using just the AVR libraries without the Pololu libraries.
+ * We will set values in the port A register and need to set
+ * the data direction register for output.
+ *
+ * _SFR_IO8 is a macro defined in sfr_defs.h to make special function
+ * register definitions look like C variables or simple constants.
+ * 
+ * These values are defined in iom1284p.h:
+ *  PINA 	_SFR_IO8(0x00)	the pin input register for A
+ *  DDRA 	_SFR_IO8(0x01)	the data direction register for A
+ *  PORTA 	_SFR_IO8(0x02) 	the port data register for A
+ *
+ */
+
+int main() { 
+	//volatile unsigned char* pinRegister = (unsigned char*)&PINA;
+	volatile unsigned char* portRegister = (unsigned char*)&PORTA;
+	volatile unsigned char* ddrRegister = (unsigned char*)&DDRA;
+	int i;
+	
+	// set data direction to 'out' for all bits in this register
+	*(ddrRegister) |= 0xFF;
+	
+	while(1) { 
+		// set the first bit to '1' to light the led
+		*(portRegister) = 1;
+		
+		// shift the bit along the register to light each of hte pins in turn
+		for(i = 0; i < 8; ++i) {
+			delay_ms(100);
+			*(portRegister) <<= 1;
+		}
+	} 
+}
+ 
