2dd739583e26acf4b8cf382147412123d7b5c616
Joseph Briggs <joseph.g.briggs@acm.org>
2014-03-09 20:30:31 -0500

    change rate of red light to 2 Hz

--- minilab/minilab.c
+++ minilab/minilab.c
@@ -16,10 +16,13 @@
  *       1 sec        8 ticks   2500 counts     1 sec
  */
 
+#define ISR_TOP		2500
+#define RED_TOP 	500
+#define LIGHT_ON_MS	100
+
 // need global variables to communicate between the ISR and the main program
-static volatile int iterationCounter;
-static volatile short ledState;
-static volatile short readyFlag;
+static volatile int redReadyCounter;
+static volatile short redReadyFlag;
 
 int main() { 
 	
@@ -34,17 +37,18 @@ int main() {
 	 * Match interrupt is enabled. */
 	TIMSK3 |= 1 << OCIE3A;
 	
-	// set the timer3 counter compare register to fire the interrupt every '2500' counts
-	OCR3A = 2500;
+	// set the timer3 counter compare register to fire when it reaches this value
+	OCR3A = ISR_TOP;
 	
 	// set the I-flag in the Status Register (interrupts globally enabled), 
 	sei();
 	
 	while (1) {
-		if (readyFlag) {
-			ledState = (ledState == 1) ? 0 : 1;
-			red_led(ledState);
-			readyFlag = 0;
+		if (redReadyFlag) {
+			red_led(1);
+			delay_ms(LIGHT_ON_MS);
+			red_led(0);
+			redReadyFlag = 0;
 		}
 	}
 }	
@@ -52,11 +56,12 @@ int main() {
 // 'ISR' special construct to create an interrupt service routine
 // 'TIMER3_COMPA_vect' is what ties this function to the Timer3 OC3A interrupt
 ISR(TIMER3_COMPA_vect) {
-	if (iterationCounter >= 1000) {
-		readyFlag = 1;
-		iterationCounter = 0;
+	// trigger the red light
+	redReadyCounter++;
+	if (redReadyCounter > RED_TOP) {
+		redReadyFlag = 1;
+		redReadyCounter = 0;
 	}
-	iterationCounter++;
 }
 
  
